54cdc7ccfda7b02f46afa84ad0f0e781daf97cae
jinhok96 <jinhok96a@gmail.com>
2024-08-12 21:44:17 +0900

    chore: MoneyInput 타입에서 중복된 formatter 타입 제거 	modified:   src/components/common/inputs/MoneyInput.tsx

--- src/components/common/inputs/MoneyInput.tsx
+++ src/components/common/inputs/MoneyInput.tsx
@@ -4,7 +4,7 @@ import Input, { InputProps } from '@/components/common/inputs/Input';
 import { REGEX } from '@/libs/constants/regex';
 import { formatNumberAddCommas } from '@/libs/utils/formatNumber';
 
-type MoneyInputProps<T extends FieldValues> = Omit<InputProps<T>, 'type' | 'regex'>;
+type MoneyInputProps<T extends FieldValues> = Omit<InputProps<T>, 'type' | 'regex' | 'formatter'>;
 
 export default function MoneyInput<T extends FieldValues>({ ...restInputProps }: MoneyInputProps<T>) {
   return <Input {...restInputProps} regex={REGEX.MONEY} type="string" formatter={formatNumberAddCommas} />;
