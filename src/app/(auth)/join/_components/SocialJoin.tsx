d6b6e0f41fcc7a9217f7cffb3362b70500f5a380
jinhok96 <jinhok96a@gmail.com>
2024-10-05 22:01:40 +0900

    chore: 회원가입 페이지에 소셜 로그인 추가 	modified:   src/app/(auth)/join/_components/SocialJoin.tsx

--- src/app/(auth)/join/_components/SocialJoin.tsx
+++ src/app/(auth)/join/_components/SocialJoin.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import Button from '@/components/common/buttons/Button';
 import KakaoSvg from '@/icons/kakao.svg';
+import { ENV } from '@/libs/constants/env';
 
 export default function SocialLogin() {
+  const kakaoRedirectURI = ENV.PROD !== ENV.KEY_PROD ? ENV.KAKAO_REDIRECT_URI_DEV : ENV.KAKAO_REDIRECT_URI;
+  const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${ENV.KAKAO_REST_API_KEY}&redirect_uri=${kakaoRedirectURI}`;
+
+  const handleClick = () => {
+    window.location.href = kakaoLoginUrl;
+  };
+
   return (
     <div className="flex-row-center justify-center">
-      <KakaoSvg className="size-10 cursor-pointer" />
+      <Button onClick={handleClick}>
+        <KakaoSvg className="size-12" />
+      </Button>
     </div>
   );
 }
