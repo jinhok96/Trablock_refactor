d58749552eb1bd8c5a12544d549b5498cf110028
jinhok96 <jinhok96a@gmail.com>
2024-11-07 03:56:29 +0900

    style: 패딩 수정 	modified:   src/components/common/dropdowns/DropdownItem.tsx

--- src/components/common/dropdowns/DropdownItem.tsx
+++ src/components/common/dropdowns/DropdownItem.tsx
@@ -28,7 +28,7 @@ export default function DropdownItem<T extends string>({
   return (
     <li {...liProps} className={`hover:bg-primary-02 ${selected && 'bg-gray-02 hover:bg-gray-02'}`}>
       <Button
-        className={`font-btn-text size-full gap-1.5 py-2 pl-4 pr-6 ${align === 'left' && '!justify-start'} ${align === 'right' && '!justify-end'} ${className}`}
+        className={`font-btn-text size-full gap-1.5 py-3 pl-4 pr-6 ${align === 'left' && '!justify-start'} ${align === 'right' && '!justify-end'} ${className}`}
         onClick={onClick}
       >
         <div className={`size-4 ${iconClassName} ${!icon && 'hidden'}`}>{icon}</div>
