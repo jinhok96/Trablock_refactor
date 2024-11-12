5973f1c271fba3986cb129f0dcde7c7d3532c63b
jinhok96 <jinhok96a@gmail.com>
2024-11-12 16:46:58 +0900

    style: 공통 마진을 각 레이아웃으로 이동, 홈에서 공통 마진 제거 	modified:   src/app/(dashboard)/layout.tsx 	modified:   src/app/(dashboard)/profile/[userId]/layout.tsx 	modified:   src/app/(dashboard)/search/layout.tsx

--- src/app/(dashboard)/search/layout.tsx
+++ src/app/(dashboard)/search/layout.tsx
@@ -6,7 +6,7 @@ type SearchPageLayoutProps = {
 
 export default function SearchPageLayout({ children }: SearchPageLayoutProps) {
   return (
-    <div className="px-layout">
+    <div className="px-layout mt-7 md:mt-10">
       <div className="m-auto max-w-screen-xl">{children}</div>
     </div>
   );
