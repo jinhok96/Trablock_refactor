b600e1acebbe3bfc45ad66f175755c5a210b4eaa
jinhok96 <jinhok96a@gmail.com>
2024-11-22 18:01:36 +0900

    chore: NextImage에서 서버용 NextServerImage 분리 	modified:   src/components/common/NextImage.tsx 	new file:   src/components/common/NextServerImage.tsx

--- src/components/common/NextServerImage.tsx
+++ src/components/common/NextServerImage.tsx
@@ -1,17 +1,13 @@
-'use client';
-
-import { useState } from 'react';
-
 import Image, { ImageProps } from 'next/image';
 
-export interface NextImageProps extends Omit<ImageProps, 'width' | 'height' | 'src'> {
+export interface NextServerImageProps extends Omit<ImageProps, 'width' | 'height' | 'src'> {
   placeholderClassName?: string;
   width: number;
   height: number;
   src?: ImageProps['src'];
 }
 
-export default function NextImage({
+export default function NextServerImage({
   className,
   placeholderClassName,
   src,
@@ -22,11 +18,8 @@ export default function NextImage({
   priority,
   style,
   ...restImageProps
-}: NextImageProps) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [error, setError] = useState(false);
-
-  if (!src || error) {
+}: NextServerImageProps) {
+  if (!src) {
     return (
       <div className={`overflow-hidden ${className}`}>
         <div className={`size-full bg-gray-02 ${placeholderClassName}`} />
@@ -36,7 +29,6 @@ export default function NextImage({
 
   return (
     <div className={`overflow-hidden ${className}`} style={style}>
-      <div className={`size-full bg-gray-02 ${placeholderClassName} ${isLoaded && 'hidden'}`} />
       <Image
         {...restImageProps}
         className="image-cover"
@@ -44,8 +36,6 @@ export default function NextImage({
         alt={alt}
         width={width * 2}
         height={height * 2}
-        onLoad={() => setIsLoaded(true)}
-        onError={() => setError(true)}
         loading={priority ? 'eager' : loading}
         priority={priority}
       />
