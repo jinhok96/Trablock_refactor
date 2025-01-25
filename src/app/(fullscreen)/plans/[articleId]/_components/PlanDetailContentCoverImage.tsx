f2238cacb5addef7869e92027a03ff641b6814ec
jinhok96 <59686080+jinhok96@users.noreply.github.com>
2025-01-25 17:09:52 +0900

    Refactor: 파일 인풋 공통화 및 이미지 압축 API 개선 (#45)
    * chore: 공통 컴포넌트 FileInput 추가
    	new file:   src/components/common/inputs/FileInput.tsx
    
    * chore: FileInput 적용
    	modified:   src/app/(fullscreen)/plans/[articleId]/_components/PlanDetailContentCoverImage.tsx
    	modified:   src/app/(main)/profile/[userId]/_components/ProfileContentUserInfoForm.tsx
    
    * chore: 이미지 압축 API 압출률 개선
    	modified:   src/app/api/compress-image/route.ts
    
    * chore: 이미지 압축 API 오류 정보 추가 및 타입 개선
    	modified:   src/apis/constants/errorCodes.ts
    	modified:   src/apis/services/compressImage/fetch.ts
    	modified:   src/apis/services/compressImage/type.ts
    
    * chore: 변경된 이미지 압축 API 적용
    	modified:   src/apis/services/article/writer/fetch.ts
    	modified:   src/apis/services/userProfile/writer/fetch.ts
    
    * chore: FileInput이 다른 파일 형식을 지원하도록 수정
    	modified:   src/app/(fullscreen)/plans/[articleId]/_components/PlanDetailContentCoverImage.tsx
    	modified:   src/app/(main)/profile/[userId]/_components/ProfileContentUserInfoForm.tsx
    	modified:   src/components/common/inputs/FileInput.tsx

--- src/app/(fullscreen)/plans/[articleId]/_components/PlanDetailContentCoverImage.tsx
+++ src/app/(fullscreen)/plans/[articleId]/_components/PlanDetailContentCoverImage.tsx
@@ -1,6 +1,7 @@
 import { ChangeEventHandler } from 'react';
 
 import NextClientImage from '@/components/common/images/NextClientImage';
+import FileInput from '@/components/common/inputs/FileInput';
 import Loading from '@/components/common/Loading';
 import { COLORS } from '@/libs/constants/colors';
 
@@ -23,19 +24,14 @@ export default function PlanDetailContentCoverImage({
     <div className={`relative h-[11.25rem] w-full overflow-hidden md:h-[15rem] 3xl:h-[20rem] ${className}`}>
       <NextClientImage src={src} alt="cover" sizes="(max-width: 1280px) 100vw, 90vw" priority />
       <div className={`absolute right-5 top-32 md:right-7 md:top-[11.75rem] xl:right-10 ${!isEditMode && 'hidden'}`}>
-        <input
+        <FileInput
           id="cover-image-input"
-          className="hidden"
-          type="file"
-          accept="image/*"
-          onChange={handleChangeCoverImage}
-        />
-        <label
-          htmlFor="cover-image-input"
           className="font-caption-3 btn-ghost cursor-pointer rounded-md px-3 py-2 text-center leading-none hover:bg-gray-02"
+          onChange={handleChangeCoverImage}
+          acceptImage
         >
           커버 이미지 변경
-        </label>
+        </FileInput>
       </div>
       <div className={`absolute left-0 top-0 size-full bg-overlay-light ${!isLoading && 'hidden'}`}>
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
