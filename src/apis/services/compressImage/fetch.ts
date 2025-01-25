bc0b8a9d9641f8f80f0665442f06e3e71d13d6ec
jinhok96 <jinhok96a@gmail.com>
2025-01-25 17:01:14 +0900

    chore: 이미지 압축 API 오류 정보 추가 및 타입 개선 	modified:   src/apis/constants/errorCodes.ts 	modified:   src/apis/services/compressImage/fetch.ts 	modified:   src/apis/services/compressImage/type.ts

--- src/apis/services/compressImage/fetch.ts
+++ src/apis/services/compressImage/fetch.ts
@@ -1,19 +1,53 @@
-import httpClient from '@/apis/httpClient/httpClient';
-import { PostImagePayload, PostImageResponse } from '@/apis/services/compressImage/type';
+import { ERROR_CODES } from '@/apis/constants/errorCodes';
+import httpClient, { ResponseGenericBody } from '@/apis/httpClient/httpClient';
+import { PostCompressImageResponse, PostImagePayload, PostImageResponse } from '@/apis/services/compressImage/type';
+import { ResponseWrapper } from '@/apis/types/common';
 
 const compressImageServices = {
-  // 질문에 대한 답변
-  postImage: async (payload: PostImagePayload) => {
+  postImage: async (payload: PostImagePayload): Promise<ResponseGenericBody<ResponseWrapper<PostImageResponse>>> => {
     const formData = new FormData();
     formData.append('file', payload);
 
-    const response = await httpClient().post<PostImageResponse>('/api/compress-image', {
+    const response = await httpClient().post<PostCompressImageResponse>('/api/compress-image', {
       body: formData
     });
 
-    const blob = new Blob([response.body], { type: 'image/png' });
-    const file = new File([blob], payload.name, { type: 'image/png' });
-    return file;
+    const bodyData: ResponseWrapper<PostImageResponse> = {
+      data: null,
+      error: null
+    };
+
+    if (!response.body) {
+      bodyData.error = {
+        code: 110001,
+        local_message: ERROR_CODES[110001],
+        field_errors: {}
+      };
+      return { ...response, body: bodyData };
+    }
+
+    if (!(response.body instanceof Blob)) {
+      bodyData.error = {
+        code: 110001,
+        local_message: ERROR_CODES[110001],
+        field_errors: {}
+      };
+      return { ...response, body: bodyData };
+    }
+
+    if (response.body.size >= 5 * 1024 * 1024) {
+      bodyData.error = {
+        code: 110002,
+        local_message: ERROR_CODES[110002],
+        field_errors: {}
+      };
+      return { ...response, body: bodyData };
+    }
+
+    const file = new File([response.body], payload.name, { type: 'image/jpeg' });
+    bodyData.data = file;
+
+    return { ...response, body: bodyData };
   }
 };
 
--- src/apis/services/compressImage/type.ts
+++ src/apis/services/compressImage/type.ts
@@ -2,4 +2,5 @@
 export type PostImagePayload = File;
 
 //response
+export type PostCompressImageResponse = Blob | { error: string };
 export type PostImageResponse = File;
