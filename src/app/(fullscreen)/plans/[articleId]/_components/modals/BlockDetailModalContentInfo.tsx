b519a69a950c056c2390dafae2524d892cef4d58
jinhok96 <jinhok96a@gmail.com>
2025-02-05 16:48:07 +0900

    chore: children이 없을 때 얼리 리턴으로 처리 	modified:   src/app/(fullscreen)/plans/[articleId]/_components/modals/BlockDetailModalContentInfo.tsx

--- src/app/(fullscreen)/plans/[articleId]/_components/modals/BlockDetailModalContentInfo.tsx
+++ src/app/(fullscreen)/plans/[articleId]/_components/modals/BlockDetailModalContentInfo.tsx
@@ -7,8 +7,10 @@ type BlockDetailModalContentInfoProps = {
 };
 
 export default function BlockDetailModalContentInfo({ className, label, children }: BlockDetailModalContentInfoProps) {
+  if (!children) return null;
+
   return (
-    <div className={`flex gap-2.5 ${!children && 'hidden'} ${className}`}>
+    <div className={`flex gap-2.5 ${className}`}>
       <p className="font-subtitle-2 shrink-0 leading-snug">{label}</p>
       <p className="font-caption-1 break-words leading-snug text-black-03">{children}</p>
     </div>
