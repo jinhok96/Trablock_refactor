38e34f06a939ac7363b1328a894d44693ca448eb
jinhok96 <jinhok96a@gmail.com>
2025-02-11 23:39:27 +0900

    refactor: 변경사항 적용 및 일부 코드 가독성 개선 	modified:   src/app/(fullscreen)/plans/[articleId]/_components/PlanDetailContentHeaderContent.tsx 	modified:   src/app/(main)/profile/[userId]/_components/ProfileContentTabInfo.tsx 	modified:   src/app/(main)/search/_components/AllResultContent.tsx 	modified:   src/app/(main)/search/_components/SearchResultContent.tsx 	modified:   src/components/common/cards/PlanCard.tsx

--- src/app/(main)/search/_components/AllResultContent.tsx
+++ src/app/(main)/search/_components/AllResultContent.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { InfiniteData } from '@tanstack/react-query';
 import { usePathname } from 'next/navigation';
 
-import { ResponseGenericBody } from '@/apis/httpClient/httpClient';
-import { GetArticleListResponse, GetSearchArticleListResponse } from '@/apis/services/article/reader/type';
+import { GetArticleListResponse } from '@/apis/services/article/reader/type';
 import { useGetArticleList } from '@/apis/services/article/reader/useService';
-import { ResponseWrapper, SortParam } from '@/apis/types/common';
+import { SortParam } from '@/apis/types/common';
 import PlanCardList from '@/components/common/cards/PlanCardList';
 import PlanCardShapeSelector from '@/components/common/cards/PlanCardShapeSelector';
 import SortDropdown from '@/components/common/dropdowns/SortDropdown';
 import { APP_QUERIES } from '@/libs/constants/appPaths';
-import useIntersectingState from '@/libs/hooks/useIntersectingState';
+import useInfiniteScrollList from '@/libs/hooks/useInfiniteScrollList';
 import useRouter from '@/libs/hooks/useRouter';
 import useSearchParams from '@/libs/hooks/useSearchParams';
 
@@ -24,18 +22,6 @@ type AllResultContentProps = {
   data: GetArticleListResponse;
 };
 
-function handleFlatMapList(
-  res?: InfiniteData<ResponseGenericBody<ResponseWrapper<GetSearchArticleListResponse>>, unknown>
-) {
-  if (!res) return null;
-  return res.pages.flatMap((item) => {
-    if (!item.body.data) return [];
-    const { content } = item.body.data;
-    if (!content.length) return [];
-    return content;
-  });
-}
-
 export default function AllResultContent({ params, data }: AllResultContentProps) {
   const { sort: initSort } = params;
   const { content, total_elements } = data;
@@ -44,36 +30,18 @@ export default function AllResultContent({ params, data }: AllResultContentProps
   const router = useRouter();
   const searchParams = useSearchParams();
   const [sort, setSort] = useState(initSort);
-  const [searchListResult, setSearchResultList] = useState(content);
-  const { ref: searchResultIntersectRef, isIntersecting: isSearchResultIntersecting } =
-    useIntersectingState<HTMLDivElement>();
 
-  const { fetchNextPage: fetchNextPlanListPage, hasNextPage: hasNextPlanListPage } = useGetArticleList({ sort });
+  const useGetArticleListRes = useGetArticleList({ sort });
+  const { ref: searchResultIntersectRef, list: allResultList } = useInfiniteScrollList<HTMLDivElement>(
+    content,
+    useGetArticleListRes
+  );
 
   const handleSortSelect = (value: SortParam) => {
     setSort(value);
     const newHref = pathname + '?' + searchParams.updateQuery(APP_QUERIES.SORT, value);
     router.hardPush(newHref);
   };
-
-  // 무한 스크롤
-  useEffect(() => {
-    if (!isSearchResultIntersecting) return;
-
-    const fetchNextPage = async () => {
-      if (!hasNextPlanListPage) return;
-
-      const res = await fetchNextPlanListPage();
-      if (!res?.data) return;
-
-      const newList = handleFlatMapList(res.data);
-      if (!newList) return;
-
-      setSearchResultList(newList);
-    };
-
-    fetchNextPage();
-  }, [isSearchResultIntersecting, hasNextPlanListPage]);
 
   return (
     <div>
@@ -84,7 +52,7 @@ export default function AllResultContent({ params, data }: AllResultContentProps
           <SortDropdown sort={sort} onSelect={handleSortSelect} />
         </div>
       </div>
-      <PlanCardList cardList={searchListResult} placeholder="검색 결과가 없습니다." priorityNum={content.length} />
+      <PlanCardList cardList={allResultList} placeholder="검색 결과가 없습니다." priorityNum={content.length} />
       <div ref={searchResultIntersectRef} />
     </div>
   );
