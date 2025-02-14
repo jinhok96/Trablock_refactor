68c6fd46693599db1e911ca5e22712b69b4e973f
jinhok96 <59686080+jinhok96@users.noreply.github.com>
2025-02-15 00:45:57 +0900

    feat: PlanCard 드롭다운에 여행 계획 수정 메뉴 추가 (#70)
    modified:   src/components/common/cards/PlanCard.tsx
    	modified:   src/components/common/cards/PlanCardDropdown.tsx
    	modified:   src/components/common/cards/PlanCardShapeBar.tsx
    	modified:   src/components/common/cards/PlanCardShapeCard.tsx

--- src/components/common/cards/PlanCardDropdown.tsx
+++ src/components/common/cards/PlanCardDropdown.tsx
@@ -1,22 +1,25 @@
 import KebabDropdownButton, { KebabDropdownButtonProps } from '@/components/common/buttons/KebabDropdownButton';
 import DropdownItem, { DropdownItemProps } from '@/components/common/dropdowns/DropdownItem';
+import CalendarSvg from '@/icons/calendar.svg';
 import DeleteSvg from '@/icons/trash.svg';
 import { COLORS } from '@/libs/constants/colors';
 
 export interface PlanCardDropdownProps extends Pick<KebabDropdownButtonProps, 'dropdownId'> {
-  onDeleteProfileClick: DropdownItemProps['onClick'];
+  onEditPlanClick: DropdownItemProps['onClick'];
+  onDeletePlanClick: DropdownItemProps['onClick'];
 }
 
-export default function PlanCardDropdown({ dropdownId, onDeleteProfileClick }: PlanCardDropdownProps) {
+export default function PlanCardDropdown({ dropdownId, onEditPlanClick, onDeletePlanClick }: PlanCardDropdownProps) {
   return (
     <KebabDropdownButton
       dropdownId={dropdownId}
       className="absolute right-3 top-3 size-6 md:right-4 md:top-4"
       dropdownClassName="right-3 top-9 md:right-4 md:top-10"
     >
+      <DropdownItem onClick={onEditPlanClick} icon={<CalendarSvg color={COLORS.BLACK_01} />} text="여행 계획 수정" />
       <DropdownItem
         className="text-red-01"
-        onClick={onDeleteProfileClick}
+        onClick={onDeletePlanClick}
         icon={<DeleteSvg color={COLORS.RED_01} />}
         text="여행 계획 삭제"
       />
